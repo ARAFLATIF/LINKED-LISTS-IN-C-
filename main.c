01ff062f856cfdb7700896aec793aff90baf1445
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-08-18 14:03:51 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+1. Create and Add Node to a List
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to add a node at the end of the list
+void addNode(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
