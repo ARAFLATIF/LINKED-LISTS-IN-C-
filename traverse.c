4bc672ae8099b688457268d4d80cd5e509734685
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-08-18 14:06:24 -0500

    Create traverse.c

---
+++ traverse.c
@@ -0,0 +1,9 @@
+// Function to traverse and print the linked list
+void traverseList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
